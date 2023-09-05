9f1049f8233d57139a939e6602ab9a17154225ab
Jeong-Rae <kkwjdfo@gmail.com>
2023-09-05 19:32:35 +0900

    [INIT] jdk11로 다운그레이드 초기화

---
+++ src/main/java/xyz/interfacesejong/interfaceapi/InterfaceApiApplication.java
@@ -0,0 +1,13 @@
+package xyz.interfacesejong.interfaceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterfaceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterfaceApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/interfacesejong/interfaceapi/InterfaceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.interfacesejong.interfaceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterfaceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
