adc289efe6196bdc2d6f5c813ae1a8400050f819
eugene <81062608+eugene225@users.noreply.github.com>
2023-09-13 14:20:38 +0900

    [FEAT] update, delete 구현

--- src/main/java/xyz/interfacesejong/interfaceapi/domain/board/controller/BoardController.java
+++ src/main/java/xyz/interfacesejong/interfaceapi/domain/board/controller/BoardController.java
@@ -1,5 +1,6 @@
 package xyz.interfacesejong.interfaceapi.domain.board.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import xyz.interfacesejong.interfaceapi.domain.board.dto.BoardDto;
@@ -28,7 +29,7 @@ public class BoardController {
         String content = param.get("content");
         Long user_id = Long.parseLong(param.get("user_id"));
 
-        BoardDto boardDto = new BoardDto(title, content, user_id);
+        BoardDto boardDto = new BoardDto(0L, title, content, user_id);
         boardService.save(boardDto);
 
         return ResponseEntity.ok(boardDto);
@@ -48,6 +49,29 @@ public class BoardController {
     @GetMapping("/findById")
     public ResponseEntity<BoardDto> findById(@RequestParam("id")Long id) throws Exception {
         return ResponseEntity.ok(boardService.findById(id));
+    }
+
+    // 글삭제
+    @DeleteMapping("/delete")
+    public ResponseEntity<BoardDto> delete(@RequestParam("id") Long id) throws Exception {
+        boardService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    //글수정
+    @PutMapping("/update")
+    public ResponseEntity<BoardDto> update(@RequestBody Map<String,String> param) throws Exception {
+        String idString = param.get("id");
+        Long id = Long.parseLong(idString);
+        String title = param.get("title");
+        String content = param.get("content");
+        String userIdString = param.get("userId");
+        Long userId = Long.parseLong(userIdString);
+
+        BoardDto updatedBoardDto = new BoardDto(id, title, content, userId);
+        updatedBoardDto = boardService.update(updatedBoardDto);
+
+        return ResponseEntity.ok(updatedBoardDto);
     }
 
 }
