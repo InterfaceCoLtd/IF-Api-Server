7f4a5293683a62ffd8d2d27987ca9c77d7a16692
Jeong-Rae <kkwjdfo@gmail.com>
2023-09-15 18:03:29 +0900

    [FEAT] AOP LOGGING 추가

---
+++ src/main/java/xyz/interfacesejong/interfaceapi/global/aop/LogAspect.java
@@ -0,0 +1,42 @@
+package xyz.interfacesejong.interfaceapi.global.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+
+@Component
+@Aspect
+public class LogAspect {
+    private final static Logger LOGGER = LoggerFactory.getLogger(Timer.class);
+
+    @Pointcut("execution(* xyz.interfacesejong.interfaceapi..*Controller.*(..))")
+    public void controller(){
+    }
+
+    @Pointcut("@annotation(xyz.interfacesejong.interfaceapi.global.aop.Timer)")
+    public void timer(){
+    }
+
+    @Around("controller() && timer()")
+    public Object timerAOP(ProceedingJoinPoint joinPoint) throws Throwable{
+        long start = System.currentTimeMillis();
+
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        Method method = methodSignature.getMethod();
+
+        Object proceed = joinPoint.proceed();
+
+        long end = System.currentTimeMillis();
+
+        LOGGER.info("[{}] executed time {} ms",method.getName(), end - start);
+        return proceed;
+    }
+
+}
