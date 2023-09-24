a86833e44d111c67ac344aa6dfe43beda45d9145
Jeong-Rae <kkwjdfo@gmail.com>
2023-09-24 19:43:39 +0900

    [FIX] CreateResponse 옵션부분 수정, delete에 투표 내역있을 경우 예외처리

--- src/main/java/xyz/interfacesejong/interfaceapi/domain/vote/domain/VoteOptionRepository.java
+++ src/main/java/xyz/interfacesejong/interfaceapi/domain/vote/domain/VoteOptionRepository.java
@@ -5,4 +5,6 @@ import java.util.Optional;
 
 public interface VoteOptionRepository extends JpaRepository<VoteOption, Long> {
     Optional<VoteOption> findById(Long optionId);
+
+    OptionCountMapping findCountById(Long id);
 }
