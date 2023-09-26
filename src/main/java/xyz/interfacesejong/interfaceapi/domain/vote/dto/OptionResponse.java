d91c1e556a61398893eb4579d8ddc6fc4df867f5
Jeong-Rae <kkwjdfo@gmail.com>
2023-09-26 16:38:20 +0900

    [FIX] datetime 포멧 변경

--- src/main/java/xyz/interfacesejong/interfaceapi/domain/vote/dto/OptionResponse.java
+++ src/main/java/xyz/interfacesejong/interfaceapi/domain/vote/dto/OptionResponse.java
@@ -1,6 +1,7 @@
 package xyz.interfacesejong.interfaceapi.domain.vote.dto;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.List;
 public class OptionResponse {
     private String subject;
 
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime endDateTime;
 
     private List<OptionDTO> options;
