6d9b37ec6a35752a0012d07d7b193c130eb9200a
Jeong-Rae <kkwjdfo@gmail.com>
2023-09-28 04:10:02 +0900

    [FEAT] user controller 엔드포인트 설정

--- src/main/java/xyz/interfacesejong/interfaceapi/domain/user/controller/UserController.java
+++ src/main/java/xyz/interfacesejong/interfaceapi/domain/user/controller/UserController.java
@@ -7,14 +7,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import xyz.interfacesejong.interfaceapi.domain.user.domain.User;
-import xyz.interfacesejong.interfaceapi.domain.user.dto.UserDTO;
 import xyz.interfacesejong.interfaceapi.domain.user.dto.UserInfoResponse;
+import xyz.interfacesejong.interfaceapi.domain.user.dto.UserSignUpRequest;
 import xyz.interfacesejong.interfaceapi.domain.user.service.UserService;
 import xyz.interfacesejong.interfaceapi.global.aop.Timer;
 import xyz.interfacesejong.interfaceapi.global.email.AuthEmail;
-import xyz.interfacesejong.interfaceapi.global.email.dto.AuthEmailResponse;
 
-import javax.mail.MessagingException;
 import java.util.List;
 
 @RestController
@@ -29,8 +27,8 @@ public class UserController {
     @Timer
     @PostMapping()
     @Operation(summary = "신규 유저 등록", description = "신규 유저를 생성합니다.")
-    public ResponseEntity<User> createUser(UserDTO userDTO){
-        return new ResponseEntity<>(userService.save(userDTO), HttpStatus.CREATED);
+    public ResponseEntity<User> createUser(UserSignUpRequest signUpRequest){
+        return new ResponseEntity<>(userService.saveUser(signUpRequest), HttpStatus.CREATED);
     }
 
     @Timer
