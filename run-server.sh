d7d779b19e3dc39049f5e3c92af0e76e94311d09
EC2 Default User <ec2-user@ip-172-31-37-27.ap-northeast-2.compute.internal>
2023-11-25 18:21:32 +0000

    ec2

--- run-server.sh
+++ run-server.sh
@@ -1,1 +1,8 @@
-nohup java -jar -Duser.timezone=Asia/Seoul build/libs/interface-api-0.0.1-SNAPSHOT.jar &
+#!/bin/bash
+
+TZ='Asia/Seoul'
+export TZ
+
+CURRENT_DATE=$(date +"%Y-%m-%d")
+
+nohup java -jar -Duser.timezone=Asia/Seoul build/libs/interface-api-0.0.1-SNAPSHOT.jar >> "${CURRENT_DATE}.log" 2>&1 &
