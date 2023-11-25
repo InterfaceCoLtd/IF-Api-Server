ba7e21b8227608e31fa39c9a22806381402ca085
EC2 Default User <ec2-user@ip-172-31-37-27.ap-northeast-2.compute.internal>
2023-11-25 18:02:55 +0000

    Revert "ec2"
    This reverts commit 61ff364cb676f6d4bcb963ff0f7fc200e5c53a20.

--- run-server.sh
+++ run-server.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-
-# GMT+9 시간대로 설정 (Asia/Seoul)
-TZ='Asia/Seoul'
-export TZ
-
-# 현재 날짜와 시간을 '년-월-일' 형식으로 가져옴
-CURRENT_DATE=$(date +"%Y-%m-%d")
-
-# nohup으로 Java 애플리케이션 실행하면서 로그 파일명을 현재 날짜로 설정
-nohup java -jar -Duser.timezone=Asia/Seoul build/libs/interface-api-0.0.1-SNAPSHOT.jar >> "${CURRENT_DATE}.log" 2>&1 &
+nohup java -jar -Duser.timezone=Asia/Seoul build/libs/interface-api-0.0.1-SNAPSHOT.jar &
